5833181516db47beccd41e52bcd0270717374302
NoFun <hpham15@ncstudents.niagaracollege.ca>
2023-10-26 01:16:10 -0400

    Initial Upload

--- EComBlazeWeb_Server/Program.cs
+++ EComBlazeWeb_Server/Program.cs
@@ -1,6 +1,8 @@
+using EcomBlaze_DataAccess.Data;
 using EComBlazeWeb_Server.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -17,6 +23,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
---
+++ EcomBlaze_Models/EcomBlazeModels/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcomBlaze_Models.EcomBlazeModels
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<Product> Products { get; set;}
+    }
+}
